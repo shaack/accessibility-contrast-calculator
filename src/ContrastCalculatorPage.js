e91ccf3c8d893edde1e1b7a2563a80006b0ae96f
shaack <shaack@gmail.com>
2025-09-08 14:04:21 +0200

    AI created a test page

---
+++ src/ContrastCalculatorPage.js
@@ -0,0 +1,9 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * License: MIT, see file 'LICENSE'
+ */
+export class ContrastCalculatorPage {
+    constructor(context, props = {}) {
+
+    }
+}
