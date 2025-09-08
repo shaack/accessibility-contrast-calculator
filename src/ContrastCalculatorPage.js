c09fb28988681671edb59bd696786984ded4341b
shaack <shaack@gmail.com>
2025-09-08 15:14:28 +0200

    fully working, needs some styling

--- src/ContrastCalculatorPage.js
+++
@@ -1,9 +0,0 @@
-/**
- * Author and copyright: Stefan Haack (https://shaack.com)
- * License: MIT, see file 'LICENSE'
- */
-export class ContrastCalculatorPage {
-    constructor(context, props = {}) {
-
-    }
-}
